a2bef91f2590cc65de100b2c5b4be0232e90b6b8
luxapientia <trongtruong.pham192@gmail.com>
2025-09-04 23:08:02 +0700

    Update static path in Vite configuration to point to the CentralServer public directory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-const staticPath = path.join(__dirname, '../CentralServer/public/au');
+const staticPath = path.join(__dirname, '../CentralServer/public');
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
